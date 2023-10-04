55647d36b8285dcaba7ac1e6f836c2d0ac308834
marwannour1 <marwannour20022016@gmail.com>
2023-10-04 07:07:26 -0700

    c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/*
+ * check_cycle - checks if there is an infinite loop in a single linked list
+ * @list: pointer to the begining of the single linked list
+ *
+ * Return: 0 if no loop, 1 if there is a loop
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = listint_t list;
+	listint_t *slow = listint_t list;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
