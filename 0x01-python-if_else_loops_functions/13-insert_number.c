e4c8fe9906146ab3198c0e6dbee551a854de2bac
marwannour1 <marwannour20022016@gmail.com>
2023-10-04 14:44:48 -0700

    if

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/*
+ * insert_node - djfdf
+ * @head: head
+ * @number: number
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+	new_node->n = number;
+	new_node->next = NULL;
+	if (!node || new->n < node->n)
+	{
+		new_node->next = node;
+		*head = new_node;
+		return (*head);
+	}
+	while (node)
+	{
+		if (new_node->n < node->next->n || !(node->next))
+		{
+			new_node->next = node->next;
+			node->next = new_node;
+			return (node);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
