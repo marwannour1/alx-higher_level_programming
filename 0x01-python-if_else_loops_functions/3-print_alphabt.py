d6df0784453b6d27d1685756f9ea13d88c27232d
marwannour1 <marwannour20022016@gmail.com>
2023-10-04 13:22:21 -0700

    if

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
+
 #!/usr/bin/python3
-def islower(c):
-    if (ord(c) >= ord('a') and ord(c) <= ord('z')):
-        return (True)
-    return (False)
+for i in range(ord('a'), ord('z') + 1):
+    if (i == ord('q') or i == ord('e')):
+        continue
+    print("{:c}".format(i), end='')
