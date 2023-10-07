4842c9b0eb9d714092f6b13ea6295b497afff041
marwannour1 <marwannour20022016@gmail.com>
2023-10-07 11:27:21 -0700

    if

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         print("{} - {} = {}".format(sys.argv[1], sys.argv[3], sub(int(sys.argv[1]), int(sys.argv[3]))))
     elif sys.argv[2] == '*':
         print("{} * {} = {}".format(sys.argv[1], sys.argv[3], mul(int(sys.argv[1]), int(sys.argv[3]))))
-    if sys.argv[2] == '/':
+    elif sys.argv[2] == '/':
         print("{} / {} = {}".format(sys.argv[1], sys.argv[3], div(int(sys.argv[1]), int(sys.argv[3]))))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
