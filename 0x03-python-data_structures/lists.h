8de68c304bb43584f97cad370c7636291a8c0539
marwannour1 <marwannour20022016@gmail.com>
2023-10-10 15:11:44 -0700

    data

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,6 +21,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int rec_palindrome(listiint_t **head, listint_t *end)
+int rec_palindrome(listint_t **head, listint_t *end);
 
 #endif /* LISTS_H */
