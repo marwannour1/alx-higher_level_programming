31b1ae7012a1dcf23f1eb9664f48a0be81bf1e08
marwannour1 <marwannour20022016@gmail.com>
2023-10-10 13:25:41 -0700

    data

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return (None)
-    for row in range(0, len(matrix)):
-        for col in range(0, len(matrix[row])):
-            print("{:d}".format(matrix[row][col], end='\n' if col == (len(matrix[row]) - 1) else ' '))
+    for row in matrix:
+        if len(row) == 0:
+            print()
+        for col in range(0, len(row)):
+            print("{:d}".format(row[col], end='\n'
+                                if col == len(row) - 1 else ' '))
