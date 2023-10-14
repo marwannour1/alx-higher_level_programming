095156fb2d987c8e07966bff287547147aefb977
marwannour1 <marwannour20022016@gmail.com>
2023-10-14 07:42:59 -0700

    more

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list = [replace if item == search else item for item in my_list]
+    new_list = [replace if item == search else item for item in my_list]
+    return (new_list)
