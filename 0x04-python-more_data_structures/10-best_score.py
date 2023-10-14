0c43baa15f2521e5f80ab3da3250ed296299538b
marwannour1 <marwannour20022016@gmail.com>
2023-10-14 08:48:08 -0700

    more

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    max_value = 0
+    max_key = None
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+    return (max_key)
