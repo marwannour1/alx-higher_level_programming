eab818240b7877916bc6d32c39f8d799bce197b2
marwannour1 <marwannour20022016@gmail.com>
2023-10-24 00:33:55 +0300

    e

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-def safe_print_divison(a, b):
-    try:
-        result = a / b
-    except ZeroDivisionError:
-        result = None
-    finally:
-        print("Inside result: {}".format(result))
-        return result
+def safe_print_list_integers(my_list=[], x=0):
+    i, c = 0, 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            c += 1
+        except (ValueError, TypeError):
+            pass
+        i += 1
+    print()
+    return (c)
