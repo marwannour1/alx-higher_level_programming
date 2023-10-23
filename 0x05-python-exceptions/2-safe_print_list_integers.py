14b48711b1aae6ab6974433b170ec5da3cb24ffe
marwannour1 <marwannour20022016@gmail.com>
2023-10-24 00:17:30 +0300

    e

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     index = 0
     try:
         while index < x:
-            if type(my_list[index]) == int:
+            if type(my_list[index]) == type(4):
                 print(my_list[index], end='')
             index += 1
     except IndexError:
