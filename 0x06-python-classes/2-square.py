fed75284638797f8ced79808ed8ab8aad9db0533
marwannour1 <marwannour20022016@gmail.com>
2024-01-26 21:30:42 +0200

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,18 +8,23 @@ Date: 26/1/2024
 
 
 class Square():
-    """Square class with a private attribute called size and a constructor to initialize it.""" 
+    """
+    Square class with a private attribute called size and
+    a constructor to initialize it.
+    """
 
     def __init__(self, size=0):
-        """ Constructor to initialize the private attribute size of the Square class to a given value or 0 by default 
+        """ Constructor to initialize the private attribute
+        size of the Square class to a given value or 0 by default
         Args:
             size (int): size of the square
         Raises:
             TypeError: if size is not an integer
             ValueError: if size is less than 0
         """
-        if type(size) is not int
+        if type(size) is not int:
             raise TypeError("size must be an integer")
+
         if size < 0:
             raise valueError("size must be >= 0")
         self.__size = size
