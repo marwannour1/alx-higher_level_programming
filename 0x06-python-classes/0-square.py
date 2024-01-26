2dd6cb5a5dc88e233c396c4272ffa41f8331b9cb
marwannour1 <marwannour20022016@gmail.com>
2024-01-26 19:41:57 +0200

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+This module is responsible for creating a class called Square
+
+Author: Marwan Nourledin Mohamed Farag
+Date: 26/1/2024
+"""
+
 
 class Square():
     """Square class"""
