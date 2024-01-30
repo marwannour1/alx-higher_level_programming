413bb90eab12d6a8fff52664f621ef84922963d9
marwannour1 <marwannour20022016@gmail.com>
2024-01-30 21:47:29 +0200

    task 1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This module adds two integers """
+
+def add_integer(a, b=98):
+    """ This function adds two integers
+    Args:
+        a: first integer will be casted to int if it is float
+        b: second integer will be casted to int if it is float
+
+    Returns:
+        The sum of a and b
+
+    Raises:
+        TypeError: If a or b are not integers or floats
+    """
+    if type(a) not in (float, int):
+        raise TypeError("a must be an integer")
+    if type(b) not in (float, int):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b) 
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
