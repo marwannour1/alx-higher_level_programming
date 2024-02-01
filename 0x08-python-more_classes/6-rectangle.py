1a4415f4172c0e108ae352ac5d61900e66363e27
marwannour1 <marwannour20022016@gmail.com>
2024-02-01 23:18:24 +0200

    6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -78,3 +78,4 @@ class Rectangle:
     def __del__(self):
         """Prints a message when the rectangle is deleted"""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
