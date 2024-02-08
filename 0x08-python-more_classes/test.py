8d58ea0224e772d1c3746cdbb30e30a687244169
marwannour1 <marwannour20022016@gmail.com>
2024-02-08 18:08:20 +0200

    0

--- 0x08-python-more_classes/test.py
+++ 0x08-python-more_classes/test.py
@@ -1,25 +1,3 @@
-class test():
-    def __init__(self, c=0):
-        self.__c = c
-
-    @property
-    def c(self):
-        return self.__c
-
-    @c.setter
-    def c(self, value):
-        if value > 5:
-            raise ValueError("Value must be less than 5")
-        self.__c = value
-    
-class test2(test):
-    def __init__(self):
-
-        super().__init__()
-
-
-if __name__ == "__main__":
-    t = test(10)
-    print(t.c)
-    t.c = 17
-  
+a = (1, 2)
+b = (1, 2)
+print(a is b)
