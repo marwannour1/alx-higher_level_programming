599ea26a17190e6978d097bd1c67a287cd6010d8
marwannour1 <marwannour20022016@gmail.com>
2024-02-12 23:20:44 +0200

    0

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,15 +8,18 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
+def main():
+    """ Add all arguments to a Python list, and then save them to a file """
 
+    arglist = list(sys.argv[1:])
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except Exception as e:
+        my_list = []
 
-arglist = list(sys.argv[1:])
+    my_list.extend(arglist)
+    save_to_json_file(my_list, "add_item.json")
 
-try:
-    my_list = load_from_json_file("add_item.json")
+if __name__ == "__main__":
 
-except Exception as e:
-    my_list = []
-
-my_list.extend(arglist)
-save_to_json_file(my_list, "add_item.json")
+    main()
