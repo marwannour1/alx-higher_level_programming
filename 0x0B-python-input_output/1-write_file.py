5595b3dd1cb63463d4579e02e7f1af36ffc84cea
marwannour1 <marwannour20022016@gmail.com>
2024-02-12 22:50:39 +0200

    0

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@
 def write_file(filename="", text=""):
     """ Write a string to a text file """
 
-    with open (filename, 'w', encoding='utf-8') as file:
+    with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
