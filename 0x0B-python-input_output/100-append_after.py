0eb7aa553cc78beef2fd9d4d83ea9d603194c77c
marwannour1 <marwannour20022016@gmail.com>
2024-02-13 15:35:04 +0200

    0

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -12,6 +12,6 @@ def append_after(filename="", search_string="", new_string=""):
             new_lines.append(line)
             if search_string in line:
                 new_lines.append(new_string)
-    
+
     with open(filename, "w") as f:
         f.writelines(new_lines)
