21846aa9d5e9851c7d18eceaaf894c8c86c921b0
marwannour1 <marwannour20022016@gmail.com>
2024-02-14 15:34:42 +0200

    0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,17 +15,57 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    def __setattr__(self, name, value):
-        """ This method sets the value of the attribute."""
-        if name == "width":
-            self.__width = value
-        elif name == "height":
-            self.__height = value
-        elif name == "x":
-            self.__x = value
-        elif name == "y":
-            self.__y = value
+    # def __setattr__(self, name, value):
+    #     """ This method sets the value of the attribute."""
+    #     if name == "width":
+    #         self.__width = value
+    #     elif name == "height":
+    #         self.__height = value
+    #     elif name == "x":
+    #         self.__x = value
+    #     elif name == "y":
+    #         self.__y = value
 
-    def __getattr__(self, name):
-        """ This method gets the value of the attribute."""
-        return self.__dict__[f"__{name}"]
+    # def __getattr__(self, name):
+    #     """ This method gets the value of the attribute."""
+    #     return self.__dict__[f"__{name}"]
+
+    @property
+    def width(self):
+        """This method gets the value of the width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """This method sets the value of the width attribute."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """This method gets the value of the height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """This method sets the value of the height attribute."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """This method gets the value of the x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """This method sets the value of the x attribute."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """This method gets the value of the y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """This method sets the value of the y attribute."""
+        self.__y = value
