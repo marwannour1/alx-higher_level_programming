36ae5de06e0e4741f6694a6f285df4ff276bb6ac
marwannour1 <marwannour20022016@gmail.com>
2024-02-14 15:02:08 +0200

    0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module contains the base class for all models in the application."""
+
+
+class Base:
+    """This class is the base class for all models in the application."""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """This method initializes a new instance of the Base class."""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
