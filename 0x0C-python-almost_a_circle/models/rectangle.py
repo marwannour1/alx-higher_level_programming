1aed8aed7bf7214d95c88af7932f7c42c48d9b01
marwannour1 <marwannour20022016@gmail.com>
2024-02-14 15:31:51 +0200

    0

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module contains the Rectangle class."""
+
+from typing import Any
+from models.base import Base
+
+class Rectangle(Base):
+    """This is the class for Rectangle objects."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """This method initializes a new instance of the Rectangle class."""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    def __setattr__(self, name, value):
+        """ This method sets the value of the attribute."""
+        if name == "width":
+            self.__width = value
+        elif name == "height":
+            self.__height = value
+        elif name == "x":
+            self.__x = value
+        elif name == "y":
+            self.__y = value
+
+    def __getattr__(self, name):
+        """ This method gets the value of the attribute."""
+        return self.__dict__[f"__{name}"]
