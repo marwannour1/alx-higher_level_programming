8de3afb313704948c6f286994489751f21fe44ed
marwannour1 <marwannour20022016@gmail.com>
2024-02-14 15:05:06 +0200

    0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@ class Base:
 
     def __init__(self, id=None):
         """This method initializes a new instance of the Base class."""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
