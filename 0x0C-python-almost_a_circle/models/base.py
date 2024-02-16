903931fe701077a83b1729d7a0676d14f70ed7c4
marwannour1 <marwannour20022016@gmail.com>
2024-02-16 14:51:03 +0200

    0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -52,3 +52,14 @@ class Base:
             new = cls(1)
         new.update(**dictionary)
         return new
+
+    @classmethod
+    def load_from_file(cls):
+        """This method returns a list of instances."""
+        filename = cls.__name__ + ".json"
+        if filename is None:
+            return []
+
+        with open(filename, "r") as f:
+            list_dicts = cls.from_json_string(f.read())
+            return [cls.create(**d) for d in list_dicts]
