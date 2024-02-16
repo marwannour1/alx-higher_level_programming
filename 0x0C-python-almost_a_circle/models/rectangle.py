17572721ec7b09865c2ccd4f5ff8579adc9aeb68
marwannour1 <marwannour20022016@gmail.com>
2024-02-16 14:36:23 +0200

    0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -115,3 +115,9 @@ class Rectangle(Base):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """This method returns the dictionary representation of a Rectangle
+        object."""
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -50,3 +50,8 @@ class Square(Rectangle):
                     self.x = value
                 elif key == "y":
                     self.y = value
+
+    def to_dictionary(self):
+        """ This method returns the dictionary representation of a Square
+        object. """
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
