a6bf23e3668c317f538b8643d7728f12ccceaf2b
marwannour1 <marwannour20022016@gmail.com>
2024-04-08 15:51:47 +0200

    1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
--- Script that create hbtn_0c_0 if doesn't exist else shows all databases
+-- Script that create hbtn_0c_0 if doesn't exist else shows all databases in
+-- MySQL server
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-SHOW DATABASES;
