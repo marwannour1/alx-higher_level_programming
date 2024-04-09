a2f2e5b2289891435f756a08e79a1757dc3345c2
marwannour1 <marwannour20022016@gmail.com>
2024-04-09 15:35:50 +0200

    100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,4 +7,4 @@ ALTER TABLE hbtn_0c_0.first_table
 MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE hbtn_0c_0.first_table
-MODIFY COLUMN id INT(11)
+MODIFY COLUMN id INT(11);
