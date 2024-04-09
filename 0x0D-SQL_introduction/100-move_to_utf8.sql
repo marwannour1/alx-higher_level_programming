80c3894ae927f9fd450f24a7f56a700c96a42598
marwannour1 <marwannour20022016@gmail.com>
2024-04-09 15:47:04 +0200

    100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,9 @@
 -- change to utf8mb4
 
-ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE
-utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0.first_table
-MODIFY COLUMN name VARCHAR(256) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE hbtn_0c_0.first_table
-MODIFY COLUMN id INT(11);
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
