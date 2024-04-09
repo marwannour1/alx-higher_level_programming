48e6170c00619783175a6013d79af48aef3b1907
marwannour1 <marwannour20022016@gmail.com>
2024-04-09 13:45:44 +0200

    4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
-/** Script that creates a table named 'first_table' in a database that
-is given as an argument if first_table exists do nothing **/
+-- Script that creates a table named 'first_table' in a database that
+-- is given as an argument if first_table exists do nothing **/
 
 
 CREATE TABLE IF NOT EXISTS first_table
