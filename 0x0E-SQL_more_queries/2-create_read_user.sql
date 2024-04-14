ed093fa202782764e168c9d7eddc9e9202f33c60
marwannour1 <marwannour20022016@gmail.com>
2024-04-14 23:53:16 +0200

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,3 +6,6 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege on the hbtn_0d_2 database to user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
