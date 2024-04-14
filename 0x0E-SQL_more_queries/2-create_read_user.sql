2433b5096ec2d963c14448536083e735f98db69d
marwannour1 <marwannour20022016@gmail.com>
2024-04-14 23:52:51 +0200

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,8 @@
---create database and user
-
+-- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it does not exist, or update the user if it does exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Grant SELECT privilege on the hbtn_0d_2 database to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'%';
