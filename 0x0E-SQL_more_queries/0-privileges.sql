6160bd35ef5759a95e55890151dec59786b33a9a
marwannour1 <marwannour20022016@gmail.com>
2024-04-14 23:32:20 +0200

    0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- list all privelages of user_0d_1 and user_0d_2
 
-SHOW GRANTS FOR 'user_0d_1', 'user_0d_2' @ 'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
