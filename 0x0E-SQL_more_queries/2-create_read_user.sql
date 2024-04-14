675f6f80f0bd04e1f5ddbeb3e88122bdecdcbe11
marwannour1 <marwannour20022016@gmail.com>
2024-04-14 23:59:16 +0200

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,7 +5,7 @@ DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege on the hbtn_0d_2 database to user_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 
 -- Apply the changes
 FLUSH PRIVILEGES;
