771a654034d8d49f7c76275518a664f4ea5170e8
marwannour1 <marwannour20022016@gmail.com>
2024-04-17 23:09:15 +0200

    8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 
 SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'CALIFORNIA';)
-ORDER BY cities.id;
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+ORDER BY cities.id ASC;
