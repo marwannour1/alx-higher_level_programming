82ce7e65c9b7dae7687501887ae7024138a13589
marwannour1 <marwannour20022016@gmail.com>
2024-04-20 23:42:48 +0200

    101

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,19 @@
+-- query
+
+SELECT
+    tv_shows.title AS title
+FROM
+    tv_shows
+WHERE
+    tv_shows.id NOT IN (
+        SELECT
+            tv_show_genres.show_id
+        FROM
+            tv_genres
+        JOIN
+            tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+        WHERE
+            tv_genres.name = 'Comedy'
+    )
+ORDER BY
+    title ASC;
