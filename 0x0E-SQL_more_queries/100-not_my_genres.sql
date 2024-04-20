7b7212bfccc0d3ee443579cfbb4c3e0c175fc158
marwannour1 <marwannour20022016@gmail.com>
2024-04-20 23:40:01 +0200

    100

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,19 @@
+-- query
+
+SELECT
+    tv_genres.name AS name
+FROM
+    tv_genres
+WHERE
+    tv_genres.id NOT IN (
+        SELECT
+            tv_show_genres.genre_id
+        FROM
+            tv_shows
+        JOIN
+            tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+        WHERE
+            tv_shows.title = 'Dexter'
+    )
+ORDER BY
+    tv_genres.name ASC;
