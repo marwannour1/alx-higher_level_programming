3e5b47f643b39bcaee1f67f37561237574528213
marwannour1 <marwannour20022016@gmail.com>
2024-04-20 23:28:18 +0200

    16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- query
 
 SELECT tv_shows.title, tv_genres.name
-FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
