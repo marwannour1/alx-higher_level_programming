f489186c27db08ab60805c1471d99aa95e323642
marwannour1 <marwannour20022016@gmail.com>
2024-06-20 16:41:55 +0300

    5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square') {
+module.exports = class Square extends require('./4-rectangle') {
   constructor(size) {
     super(size, size);
   }
--- 0x13-javascript_objects_scopes_closures/test.js
+++ 0x13-javascript_objects_scopes_closures/test.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./1-rectangle');
+const Square = require('./5-square');
 
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
+const s1 = new Square(4);
+s1.print();
+s1.double();
+s1.print();
