81663416216bf02def6f47222cae067e2f0ab018
marwannour1 <marwannour20022016@gmail.com>
2024-06-25 18:31:56 +0300

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ def filterStates(userName: str, password: str, dbName: str, name: str) -> None:
         db = MySQLdb.connect(host="localhost", port=3306, user=userName,
                              passwd=password, db=dbName, charset="utf8")
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name = '{}' COLLATE\
+        cur.execute("SELECT * FROM states WHERE name = {} COLLATE\
                     utf8_bin ORDER BY id ASC;".format(name))
 
         rows = cur.fetchall()
