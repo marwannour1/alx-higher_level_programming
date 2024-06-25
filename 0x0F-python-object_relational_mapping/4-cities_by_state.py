221c44ac2f6104be3937297d416e695dfc803d9f
marwannour1 <marwannour20022016@gmail.com>
2024-06-25 19:04:45 +0300

    4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+''' This module is used to select all the cities from the database'''
+import MySQLdb
+import sys
+
+
+def filterStates(userName: str, password: str, dbName: str, name: str) -> None:
+    ''' This function lists all the cities'''
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=userName,
+                             passwd=password, db=dbName, charset="utf8")
+        cur = db.cursor()
+        query = "SELECT state_id, cities.id, cities.name FROM cities\
+                JOIN states ON cities.state_id = states.id\
+                ORDER BY cities.id ASC;"
+        cur.execute(query, (name, ))
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+
+    finally:
+        cur.close()
+        db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 5:
+        filterStates(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    else:
+        print("Usage: {} username password database name".format(sys.argv[0]))
